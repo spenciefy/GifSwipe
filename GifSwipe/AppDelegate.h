63e25095ea7c459e86bb7f591a5f63e6e8ca14cf
Spencer Yen <spencersyen@gmail.com>
2014-12-28 20:03:37 -0800

    Initial Commit

---
+++ GifSwipe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GifSwipe
+//
+//  Created by Spencer Yen on 12/28/14.
+//  Copyright (c) 2014 Parameter Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
